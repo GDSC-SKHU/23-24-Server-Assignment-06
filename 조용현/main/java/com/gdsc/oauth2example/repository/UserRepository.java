fb3544843ea3e6806c3df7e32d7d3a990f6829d4
Cho, Yonghyun <chooh1010@gamil.com>
2023-11-26 21:43:56 +0900

    수정

--- 조용현/main/java/com/gdsc/oauth2example/repository/UserRepository.java
+++ 조용현/main/java/com/gdsc/oauth2example/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.gdsc.oauth2example.repository;
 import com.gdsc.oauth2example.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

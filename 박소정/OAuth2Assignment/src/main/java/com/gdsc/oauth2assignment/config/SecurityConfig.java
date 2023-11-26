0d7ad3cf26c9556efe0504225a7c2e55fe1ca6fb
Park, Sojeong <sojeong2101@gmail.com>
2023-11-26 14:06:27 +0900

    feat: SecurityConfig 구현

---
+++ 박소정/OAuth2Assignment/src/main/java/com/gdsc/oauth2assignment/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.gdsc.oauth2assignment.config;
+
+import com.gdsc.oauth2assignment.jwt.JwtFilter;
+import com.gdsc.oauth2assignment.jwt.TokenProvider;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable).sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable).logout(AbstractHttpConfigurer::disable) // 폼 로그인, 로그아웃 비활성화
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests.requestMatchers("/login/oauth2/**").permitAll()
+                                .requestMatchers("후에 구현 할 글쓰기 API 넣기").authenticated().anyRequest().authenticated())
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}

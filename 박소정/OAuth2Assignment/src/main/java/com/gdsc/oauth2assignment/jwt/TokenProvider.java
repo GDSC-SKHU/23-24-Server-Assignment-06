97461fea1b8703661b9f062eb271e79b993693c7
Park, Sojeong <sojeong2101@gmail.com>
2023-11-26 12:18:40 +0900

    feat: parseClaims(), 클레임 피싱 기능 구현

--- 박소정/OAuth2Assignment/src/main/java/com/gdsc/oauth2assignment/jwt/TokenProvider.java
+++ 박소정/OAuth2Assignment/src/main/java/com/gdsc/oauth2assignment/jwt/TokenProvider.java
@@ -2,14 +2,16 @@ package com.gdsc.oauth2assignment.jwt;
 
 import com.gdsc.oauth2assignment.domain.User;
 import com.gdsc.oauth2assignment.dto.Token;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import java.security.Key;
 @Component
 public class TokenProvider {
     private final Key key; // JWT를 암호화하기 위한 비밀 키를 저장, Key는 인터페이스로 구체적인 키 값이 할당됨.
@@ -37,5 +39,21 @@ public class TokenProvider {
         return Token.builder()
                 .accessToken(accessToken)
                 .build(); // Token 객체로 반환
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+
+        /*
+         * Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+         * : 클레임 추출
+         * Jwts.parserBuilder().setSigningKey(key).build() -> 파싱하는 빌더 생성, 서명 키 설정
+         * parseClaimsJws(accessToken) -> 주어진 액세스 토큰 파싱
+         * getBody() -> JWT에서 클레임 가져옴
+         * */
     }
 }
